bb44e8df7b57ce1bb3f34722b9b6ed735e7124fc
Max von Webel <max@343max.de>
2018-10-22 20:42:50 -0700

    removed p2.oauth2 pod

--- Podfile
+++ Podfile
@@ -4,7 +4,6 @@ target 'Mactodon' do
   use_frameworks!
 
   pod 'MastodonKit', :git => 'https://github.com/MastodonKit/MastodonKit.git', :branch => 'master'
-  pod 'p2.OAuth2', :git => 'https://github.com/p2/OAuth2', :submodules => true
 
   target 'MactodonTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-Mactodon/Pods-Mactodon-frameworks.sh
+++ Pods/Target Support Files/Pods-Mactodon/Pods-Mactodon-frameworks.sh
@@ -144,11 +144,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MastodonKit/MastodonKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/p2.OAuth2/p2_OAuth2.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MastodonKit/MastodonKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/p2.OAuth2/p2_OAuth2.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
