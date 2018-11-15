31dba5a9dbfa3a893bb2ff566efb99b2b5e628f4
Max von Webel <max@343max.de>
2018-11-14 20:58:00 -0800

    added a follow button to the follower notifications, not working for now

--- Mactodon/FeedRendering/FollowingItem.swift
+++ Mactodon/FeedRendering/FollowingItem.swift
@@ -10,6 +10,7 @@ class FollowingItem: NSCollectionViewItem, FeedViewCell {
   
   private var actorAvatar: AvatarView!
   private var descriptionView: NSTextView!
+  private var actionButton: NSButton!
   
   func willDisplay() {
     guard let account = account else {
@@ -44,10 +45,24 @@ class FollowingItem: NSCollectionViewItem, FeedViewCell {
     descriptionView = NSTextView(frame: .zero)
     descriptionView.prepareAsLabel()
     view.addSubview(descriptionView)
+    
+    actionButton = NSButton(title: "Follow", target: self, action: #selector(clickedFollowButton(_:)))
+    actionButton.bezelStyle = .roundRect
+    actionButton.font = NSFont.systemFont(ofSize: NSFont.systemFontSize(for: .small))
+    view.addSubview(actionButton)
+  }
+  
+  @IBAction func clickedFollowButton(_ sender: NSButton) {
+    print("follow!")
   }
   
   func layout(width: CGFloat) {
     CellLayout.layoutActorRow(hasActorRow: true, width: width, avatar: actorAvatar, description: descriptionView)
+    
+    actionButton.sizeToFit()
+    var buttonFrame = view.frame.alignedRect(size: actionButton.frame.size, horizontalOffset: 1.0, verticalOffset: 0.5)
+    buttonFrame.origin.x -= CellLayout.margin.right
+    actionButton.frame = buttonFrame
   }
   
   override func viewDidLayout() {
